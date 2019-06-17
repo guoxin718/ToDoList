c450951d31666989526ec4c1ea361c3cb8e1c058
guoxin718 <guoxin718@gmail.com>
2019-06-17 14:19:12 +0800

    Add files via upload

---
+++ ToDoList/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ToDoList
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnDesktopBk = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(66, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(480, 250);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(653, 37);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(480, 250);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.Location = new System.Drawing.Point(66, 350);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(480, 250);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox4.Location = new System.Drawing.Point(653, 348);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(480, 250);
+            this.textBox4.TabIndex = 2;
+            // 
+            // btnDesktopBk
+            // 
+            this.btnDesktopBk.Location = new System.Drawing.Point(562, 300);
+            this.btnDesktopBk.Name = "btnDesktopBk";
+            this.btnDesktopBk.Size = new System.Drawing.Size(75, 33);
+            this.btnDesktopBk.TabIndex = 1;
+            this.btnDesktopBk.Text = "保存";
+            this.btnDesktopBk.UseVisualStyleBackColor = true;
+            this.btnDesktopBk.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 144);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "十万火急";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(1148, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 144);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "临时保存";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(12, 354);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 144);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "缓缓再做";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(1148, 354);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 144);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "长期闲置";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 652);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnDesktopBk);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ToDoList";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button btnDesktopBk;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ToDoList/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			textBox1.Text = File.ReadAllText(@"D:\\dev_vs\\ToDoList\\1.txt", Encoding.UTF8);
+			textBox2.Text = File.ReadAllText(@"D:\\dev_vs\\ToDoList\\2.txt", Encoding.UTF8);
+			textBox3.Text = File.ReadAllText(@"D:\\dev_vs\\ToDoList\\3.txt", Encoding.UTF8);
+			textBox4.Text = File.ReadAllText(@"D:\\dev_vs\\ToDoList\\4.txt", Encoding.UTF8);
+		}
+
+		public void WriteToFile()
+		{
+			//如果文件不存在，则创建；存在则覆盖
+			System.IO.File.WriteAllText(@"D:\\dev_vs\\ToDoList\\1.txt", textBox1.Text, Encoding.UTF8);
+			System.IO.File.WriteAllText(@"D:\\dev_vs\\ToDoList\\2.txt", textBox2.Text, Encoding.UTF8);
+			System.IO.File.WriteAllText(@"D:\\dev_vs\\ToDoList\\3.txt", textBox3.Text, Encoding.UTF8);
+			System.IO.File.WriteAllText(@"D:\\dev_vs\\ToDoList\\4.txt", textBox4.Text, Encoding.UTF8);
+		}
+
+		[DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+		public static extern int SystemParametersInfo(
+		int uAction,
+		int uParam,
+		string lpvParam,
+		int fuWinIni
+		);
+
+		private void BtnSave_Click(object sender, EventArgs e)
+		{
+            WriteToFile();
+            System.Drawing.Image imgSrc = System.Drawing.Image.FromFile("D:\\dev_vs\\ToDoList\\template1.jpg");
+
+			using (Graphics g = Graphics.FromImage(imgSrc))
+			{
+				g.DrawImage(imgSrc, 0, 0, imgSrc.Width, imgSrc.Height);
+				using (Font f = new Font("微软雅黑", 30))
+				{
+					using (Brush b = new SolidBrush(Color.Black))
+					{
+						int x = 720, y = 250, h = 1368, w = 768;
+						g.DrawString(textBox1.Text, f, b, new Rectangle(x + 30, y + 10, w, h));
+						g.DrawString(textBox2.Text, f, b, new Rectangle(x + 990, y + 10, w, h));
+						g.DrawString(textBox3.Text, f, b, new Rectangle(x + 30, y + 550, w, h));
+						g.DrawString(textBox4.Text, f, b, new Rectangle(x + 990, y + 550, w, h));
+					}
+				}
+			}
+
+			imgSrc.Save("D:\\dev_vs\\ToDoList\\new.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+			SystemParametersInfo(20, 0, "D:\\dev_vs\\ToDoList\\new.jpg", 0x2);
+		}
+
+        private void NotifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            ////还原窗体显示    
+            //WindowState = FormWindowState.Normal;
+            ////激活窗体并给予它焦点
+            //this.Activate();
+            ////任务栏区显示图标
+            //this.ShowInTaskbar = true;
+            ////托盘区图标隐藏
+            //notifyIcon1.Visible = false;
+
+            if (e.Button == MouseButtons.Left)//判断鼠标的按键
+            {
+                if (this.WindowState == FormWindowState.Normal)
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                    this.Hide();
+                }
+                else if (this.WindowState == FormWindowState.Minimized)
+                {
+                    this.Show();
+                    this.WindowState = FormWindowState.Normal;
+                    this.Activate();
+                }
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                if (MessageBox.Show("是否需要关闭程序？", "提示:", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation) == DialogResult.OK)//出错提示
+                {
+                    //关闭窗口
+                    DialogResult = DialogResult.No;
+                    Dispose();
+                    Close();
+                }
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;//取消窗体的关闭事件
+            this.WindowState = FormWindowState.Minimized;//使当前窗体最小化
+            notifyIcon1.Visible = true;//使最下滑的图标可见
+
+            //if (MessageBox.Show("是否确认退出程序？", "退出", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            //{
+            //    // 关闭所有的线程
+            //    this.Dispose();
+            //    this.Close();
+            //}
+            //else
+            //{
+            //    e.Cancel = true;
+            //}
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            //判断是否选择的是最小化按钮
+            if (WindowState == FormWindowState.Minimized)
+            {
+                //隐藏任务栏区图标
+                this.ShowInTaskbar = false;
+                //图标显示在托盘区
+                notifyIcon1.Visible = true;
+            }
+        }
+    }
+}
